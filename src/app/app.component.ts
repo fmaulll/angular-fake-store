a9ac49a1337ec0182f49529b94892798ec843f69
fmaulll <maul2821@gmail.com>
2023-05-18 01:51:06 +0700

    ini

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component'; 
+import { HomeComponent } from './home/home.component';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
+  imports: [
+    CommonModule,
+    HeaderComponent,
+    HomeComponent
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
