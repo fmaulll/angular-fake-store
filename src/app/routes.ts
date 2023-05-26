015f37927a7be9b3d229dfca54683ef036e78e25
fmaulll <maul2821@gmail.com>
2023-05-26 18:04:29 +0700

    added cart routes

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
+import { CartComponent } from './cart/cart.component';
 
 const routeConfig: Routes = [
     {
@@ -12,6 +13,11 @@ const routeConfig: Routes = [
       path: 'details/:id',
       component: DetailsComponent,
       title: 'Product Details'
+    },
+    {
+      path: 'cart',
+      component: CartComponent,
+      title: 'Cart'
     }
   ];
   
