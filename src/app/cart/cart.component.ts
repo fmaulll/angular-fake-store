22020c1e0474dd9ad9347d1da5b4c18b21e5c48a
fmaulll <maul2821@gmail.com>
2023-05-27 14:12:42 +0700

    cart components update

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CartItem } from '../product';
 
 @Component({
+  standalone: true,
+  imports: [CommonModule],
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
 })
 export class CartComponent {
+  cartList: CartItem[] = []
 
+  constructor() {
+    this.cartList = JSON.parse(localStorage.getItem('cart')!)
+    console.log(this.cartList)
+  }
 }
--- src/app/product.ts
+++ src/app/product.ts
@@ -8,5 +8,19 @@ export interface Product {
   rating: {
     rate: number;
     count: number;
-  }
+  };
+}
+export interface CartItem {
+  id: number;
+  title: string;
+  price: number;
+  category: string;
+  description: string;
+  image: string;
+  rating: {
+    rate: number;
+    count: number;
+  };
+  quantity: number;
+  total: number;
 }
