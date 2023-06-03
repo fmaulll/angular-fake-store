d87e114a0c8ce3307c385ddaea6f24af957d363a
fmaulll <maul2821@gmail.com>
2023-06-03 13:22:51 +0700

    cart items for now

--- src/app/cart-item/cart-item.component.ts
+++ src/app/cart-item/cart-item.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, Input } from '@angular/core';
+import { CartItem } from '../product';
+import { ProductService } from '../product.service';
 
 @Component({
+  standalone: true,
+  imports: [CommonModule],
   selector: 'app-cart-item',
   templateUrl: './cart-item.component.html',
-  styleUrls: ['./cart-item.component.scss']
+  styleUrls: ['./cart-item.component.scss'],
 })
 export class CartItemComponent {
+  @Input() cartItem!: CartItem;
+  productService: ProductService = inject(ProductService);
+  quantity: number = 1;
 
+  ngOnInit() {
+    this.quantity = this.cartItem.quantity
+  }
+
+  constructor() {}
+
+  increment() {
+    this.quantity += 1;
+  }
+
+  decrement() {
+    if (this.quantity === 1) {
+      return;
+    }
+    this.quantity -= 1;
+  }
 }
--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CartItemComponent } from '../cart-item/cart-item.component';
 import { CartItem } from '../product';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CartItemComponent],
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.scss']
